fc89a1c0a36588ba15490439a679af890160e0c4
Timothy Small <smalls.online@icloud.com>
2023-01-26 13:04:09 -0500

    Added IsExternalInit when targeting netstandard2.1

---
+++ src/LAPS.Lib/IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿#if NETSTANDARD2_1
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit
+{ }
+#endif
